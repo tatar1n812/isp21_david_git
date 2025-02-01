fc4113ab6ae286bd35d60b166153393b22e4399a
david <chekastiy@mail.ru>
2025-02-01 10:31:56 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите cвое имя: ");
+string? name = Console.ReadLine();
+Console.WriteLine($"Поздравляю дорогой {name} c твоим днем рождения");
+Console.WriteLine("Желаю Уверенности в себе!");
+Console.WriteLine("Больших успехов!");
+Console.WriteLine("Внимания и заботы!");
+Console.WriteLine("Побольше свободного времени!");
+Console.WriteLine("И удачи во всех начинаниях!");
+Console.WriteLine("С наилучшими пожеланиями от Давид");
+
